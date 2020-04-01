b816e111e2d85688487737e95dd82b1412d0a84e
gamingdiamond982 <30845740+gamingdiamond982@users.noreply.github.com>
2020-04-01 23:32:40 +0100

    Update commands.py

--- src/commands.py
+++ src/commands.py
@@ -1,1 +1,15 @@
 
+
+def parseMessage(message, author):
+  return
+  
+  
+
+
+Commands = {
+  'reference' = (
+    'reference',
+    'Prints a message containing all of the commands',
+    
+  )
+}
