a940451a58ff0d44aebaf2dbe456498d990ac232
gamingdiamond982 <30845740+gamingdiamond982@users.noreply.github.com>
2020-04-01 22:14:16 +0100

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,8 @@
 import discord
 import json
 
+client = discord.Client()
+
 class BotError(Exception):
   pass
 
@@ -16,4 +18,21 @@ def parseConfig():
   except KeyError:
     raise BotError("Bad config.json file")
     
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  #TODO: add command parsing
+  
+if __name__ == '__main__':
+  token, prefix = parseConfig()
+  client.run(token)
+  
+      
+
     
+  
