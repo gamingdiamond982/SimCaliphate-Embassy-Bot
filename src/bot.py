790eb56d7788ebb6fa7a637608fb281301ed048a
gamingdiamond982 <30845740+gamingdiamond982@users.noreply.github.com>
2020-04-01 21:51:17 +0100

    Basic config file

--- src/bot.py
+++ src/bot.py
@@ -1,1 +1,19 @@
 import discord
+import json
+
+class BotError(Exception):
+  pass
+
+
+def parseConfig():
+  try:
+    config = open('config.json', 'r')
+    configDict = json.load(config)
+    config.close()
+    token = configDict["token"]
+    prefix = configDict["prefix"]
+    return token, prefix
+  except KeyError:
+    raise BotError("Bad config.json file")
+    
+    
