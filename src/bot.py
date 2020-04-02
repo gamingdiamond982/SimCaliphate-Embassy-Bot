e5a78bb6a7be503f96e08a73a546ce9b7a5b2b10
gamingdiamond982 <30845740+gamingdiamond982@users.noreply.github.com>
2020-04-02 19:22:38 +0100

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -32,24 +32,24 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    if message.content.startswith(prefix):
 
-        if message.content.startswith(prefix):
+        # get the message content
+        content = message.content
 
-            # get the message content
-            content = message.content
+        # get rid of the prefix
+        command_content = content[len(prefix):].lstrip()
+        response = commands.parseMessage(command_content, message.author)
 
-            # get rid of the prefix
-            command_content = content[len(prefix):].lstrip()
-            response = commands.parseMessage(command_content, message.author)
-
-            chunks = utils.split_into_chunks(response.encode('utf-8'), 1024)
-            embed = discord.Embed(color=0xb00c0c)
-            title = command_content.split()[0] if len(
-                command_content.split()) > 0 else 'Empty Message'
-            for i, chunk in enumerate(chunks):
-                title = "(cont'd)" if i > 0 else title
-                embed.add_field(name=title, value=chunk.decode(
-                    'utf-8'), inline=False)
+        chunks = utils.split_into_chunks(response.encode('utf-8'), 1024)
+        embed = discord.Embed(color=0xb00c0c)
+        title = command_content.split()[0] if len(
+            command_content.split()) > 0 else 'Empty Message'
+        for i, chunk in enumerate(chunks):
+             title = "(cont'd)" if i > 0 else title
+             embed.add_field(name=title, value=chunk.decode(
+                'utf-8'), inline=False)
             embed.set_thumbnail(url=message.author.avatar_url)
             embed.set_footer(text="Long live the Sultan")
             await message.channel.send(embed=embed)
