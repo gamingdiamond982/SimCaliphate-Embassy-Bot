f9099117eeb9b38081cb4d011cd8f6dfd5eb1343
gamingdiamond982 <30845740+gamingdiamond982@users.noreply.github.com>
2020-04-02 18:04:35 +0100

    Update commands.py

--- src/commands.py
+++ src/commands.py
@@ -1,15 +1,27 @@
+from bot import client
+import asyncio
 
+async def assertAuthLevel(author):
+	
+	
 
 def parseMessage(message, author):
-  return
-  
-  
+	content = message.split() #splits message at spaces
+	if content[0] in Commands: #if the first word of the message is in Commands:
+		return Commands[content[0]][len(Commands[content])-1](message, author) #Gets the function for the command
+	else:
+		return 'Invalid Command'
 
+def parse_reference(content, author):
+	return
+def reference(command, author):
+	#TODO: implement reference command
+	return
 
 Commands = {
-  'reference' = (
-    'reference',
-    'Prints a message containing all of the commands',
-    
-  )
+	'reference' = (
+		'reference',
+		'Prints a message containing all of the commands',
+		parse_reference
+  	)
 }
