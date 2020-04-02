17a18ff419467485f6352d6cf8bbe86958c814a8
Bilal <bilal.a.haroon@gmail.com>
2020-04-02 13:04:12 -0500

    got rid of unnesacesarry code

--- src/commands.py
+++ src/commands.py
@@ -2,28 +2,27 @@ from bot import client
 import asyncio
 
 
-def parseMessage(message, author):
-    content = message.split()  # splits message at spaces
-    # if the first word of the message is in Commands:
-    if content[0] in Commands:
-        # Gets the function for the command
-        return Commands[content[0]][len(Commands[content])-1](message, author)
-    else:
-        return 'Invalid Command'
+# def parseMessage(message, author):
+#     content = message.split()  # splits message at spaces
+#     # if the first word of the message is in Commands:
+#     if content[0] in Commands:
+#         # Gets the function for the command
+#         return Commands[content[0]][len(Commands[content])-1](message, author)
+#     else:
+#         return 'Invalid Command'
 
 
-def parse_reference(content, author):
+def parseMessage(content, author):
     for cmd in Commands:
         command_name = Commands[cmd][0]
         command_desc = Commands[cmd][1]
         return command_name + " " + command_desc
 
 
-
 Commands = {
     'reference': (
         'reference',
         'Prints a message containing all of the commands',
-        parse_reference
+        parseMessage
     )
 }
