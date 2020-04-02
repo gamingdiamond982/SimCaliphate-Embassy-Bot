2393f9e98240fabc400b7fde58dfa2f61ab190f9
Bilal <bilal.a.haroon@gmail.com>
2020-04-02 12:13:31 -0500

    typo

--- src/bot.py
+++ src/bot.py
@@ -5,53 +5,52 @@ import json
 
 client = discord.Client()
 
+
 class BotError(Exception):
-  pass
+    pass
 
 
 def parseConfig():
-  """reads the confic.json file"""
-  try:
-    config = open('config.json', 'r')
-    configDict = json.load(config)
-    config.close()
-    token = configDict["token"]
-    prefix = configDict["prefix"]
-    return token, prefix
-  except KeyError:
-    raise BotError("Bad config.json file")
-    
+    """reads the confic.json file"""
+    try:
+        config = open('config.json', 'r')
+        configDict = json.load(config)
+        config.close()
+        token = configDict["token"]
+        prefix = configDict["prefix"]
+        return token, prefix
+    except KeyError:
+        raise BotError("Bad config.json file")
+
+
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
 
 """Main message loop"""
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-	if message.content.startswith(prefix):
-    	content = message.content
-    	command_content = content[len(prefix):].lstrip()
-    	response = commands.parseMessage(command_content, message.author)
-    
-    	chunks = utils.split_into_chunks(response.encode('utf-8'), 1024)
-			embed = discord.Embed(color=0xb00c0c)
-    	title = command_content.split()[0] if len(command_content.split()) > 0 else 'Empty Message'
-    	for i, chunk in enumerate(chunks):
-			title = "(cont'd)" if i > 0 else title
-			embed.add_field(name=title, value=chunk.decode('utf-8'), inline=False)
-    	embed.set_thumbnail(url=message.author.avatar_url)
-		embed.set_footer(text="Long live the Dictator")
-    	await message.channel.send(embed=embed)
+        if message.content.startswith(prefix):
+            content = message.content
+            command_content = content[len(prefix):].lstrip()
+            response = commands.parseMessage(command_content, message.author)
+
+            chunks = utils.split_into_chunks(response.encode('utf-8'), 1024)
+            embed = discord.Embed(color=0xb00c0c)
+            title = command_content.split()[0] if len(
+                command_content.split()) > 0 else 'Empty Message'
+            for i, chunk in enumerate(chunks):
+                title = "(cont'd)" if i > 0 else title
+                embed.add_field(name=title, value=chunk.decode(
+                    'utf-8'), inline=False)
+            embed.set_thumbnail(url=message.author.avatar_url)
+            embed.set_footer(text="Long live the Dictator")
+            await message.channel.send(embed=embed)
 
 
 if __name__ == '__main__':
-	token, prefix = parseConfig()
-	client.run(token)
-  
-      
-
-    
-  
+    token, prefix = parseConfig()
+    client.run(token)
--- src/commands.py
+++ src/commands.py
@@ -2,26 +2,29 @@ from bot import client
 import asyncio
 
 
-	
-	
-
 def parseMessage(message, author):
-	content = message.split() #splits message at spaces
-	if content[0] in Commands: #if the first word of the message is in Commands:
-		return Commands[content[0]][len(Commands[content])-1](message, author) #Gets the function for the command
-	else:
-		return 'Invalid Command'
+    content = message.split()  # splits message at spaces
+    # if the first word of the message is in Commands:
+    if content[0] in Commands:
+        # Gets the function for the command
+        return Commands[content[0]][len(Commands[content])-1](message, author)
+    else:
+        return 'Invalid Command'
+
 
 def parse_reference(content, author):
-	return
+    return
+
+
 def reference(command, author):
-	#TODO: implement reference command
-	return
+    # TODO: implement reference command
+    return
+
 
 Commands = {
-	'reference' = (
-		'reference',
-		'Prints a message containing all of the commands',
-		parse_reference
-  	)
+    'reference': (
+        'reference',
+        'Prints a message containing all of the commands',
+        parse_reference
+    )
 }
